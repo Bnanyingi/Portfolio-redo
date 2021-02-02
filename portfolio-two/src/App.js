e7c4534d41f1e285c963498c871132cad773cff7
collinskoech11 <collinskoechck34@gmail.com>
2021-02-02 15:01:40 +0300

    debug update

--- portfolio-two/src/App.js
+++ portfolio-two/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from "react-router-dom";
  import Navbar from './components/Navbar';
  import Home from './pages/Home';
  import Contact from './pages/Contact';
+ import Projects from './pages/Projects'
 import './App.css';
 
 function App() {
