e545f2858badd570a7240b1dea5a963dcf907652
BNanyingi <nanyingibarbara@gmail.com>
2021-02-05 22:05:13 +0300

    update

--- portfolio-two/src/pages/Projects.js
+++ portfolio-two/src/pages/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props){
@@ -12,14 +12,29 @@ class Projects extends Component {
 
         if(this.state.activeTab === 0) {
             return(
-                <div><h1></h1></div>
+              <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+
+              </Card>
+                
+            )
+        }
+        else if(this.state.activeTab == 1) {
+            return(
+                <div><h1> This is React</h1></div>  
+            )
+        }
+        else if(this.state.activeTab == 2) {
+            return(
+                <div><h1> This is Angular</h1></div>  
+            )
+        }
+        else if(this.state.activeTab == 3) {
+            return(
+                <div><h1> This is HTML5</h1></div>  
             )
         }
 
     }
-
-
-
 
      render(){
      return (
@@ -33,7 +48,13 @@ class Projects extends Component {
             </Tabs>
 
             <section className="projects-grid">
-                {this.toggleCategories()}
+                <Grid className="projects-grid">
+                    <Cell col={12}>
+                       <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+
+                </Grid>
+                
             </section>
         </div>
     )
