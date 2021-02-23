072d995806c22c793c1d8dec0a6aadbe78e765c9
BNanyingi <nanyingibarbara@gmail.com>
2021-02-23 23:57:31 +0300

    update

--- portfolio-two/src/pages/Contact.js
+++ portfolio-two/src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Cell} from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 
 function Contact() {
     return (
@@ -17,7 +17,29 @@ function Contact() {
                 <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, corrupti libero ipsa eius velit quos sapiente exercitationem qui possimus, suscipit voluptas dignissimos debitis cumque natus aspernatur, in repellat distinctio accusamus?
                 </p>
                 </Cell>
-                <Cell col={6}>Half Page</Cell>
+                <Cell col={6}>
+                    <h2>Contact Me</h2>
+                    <hr/>
+
+                    <div className="contact-list">
+                      <List>
+                      <ListItem>
+                        <ListItemContent style={{fontSize: '25px', fontFamily: 'RocknRoll One'}}>
+                          <i className="fa fa-phone-square" aria-hidden="true" />
+                          (+2547)41848-472
+                        </ListItemContent>
+                      </ListItem>
+                      <ListItem>
+                        <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                      </ListItem>
+                      <ListItem>
+                        <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                      </ListItem>
+                      </List>
+                    </div>
+
+                   
+                </Cell>
 
             </Grid>
             
