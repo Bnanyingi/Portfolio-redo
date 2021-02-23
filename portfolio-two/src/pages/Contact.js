cf73992aa33a3edf927a2722fc00819e66c481f9
BNanyingi <nanyingibarbara@gmail.com>
2021-02-23 11:58:24 +0300

    update

--- portfolio-two/src/pages/Contact.js
+++ portfolio-two/src/pages/Contact.js
@@ -11,9 +11,11 @@ function Contact() {
                    
                    src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
                    alt="image1.png"
-                   style={{height: '250px'}}
+                   style={{height: '200px'}}
 
                 />
+                <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, corrupti libero ipsa eius velit quos sapiente exercitationem qui possimus, suscipit voluptas dignissimos debitis cumque natus aspernatur, in repellat distinctio accusamus?
+                </p>
                 </Cell>
                 <Cell col={6}>Half Page</Cell>
 
