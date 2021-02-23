d2f0e5084684be6828c651c35ff819f9d6caaeac
BNanyingi <nanyingibarbara@gmail.com>
2021-02-23 11:54:13 +0300

    Contact update

--- portfolio-two/src/pages/Contact.js
+++ portfolio-two/src/pages/Contact.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Cell} from 'react-mdl';
 
 function Contact() {
     return (
-        <div>
+        <div className="contact-body">
+            <Grid className="contact-grid">
+                <Cell col={6}>
+                <h2>Barbara Nanyingi</h2>
+                <img 
+                   
+                   src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                   alt="image1.png"
+                   style={{height: '250px'}}
+
+                />
+                </Cell>
+                <Cell col={6}>Half Page</Cell>
+
+            </Grid>
             
         </div>
     )
